34d1b5380e6e19608aeb9c55df03724c8b5dcfda
Archana3117 <archanamahajan1220@gmail.com>
2025-02-27 17:37:41 +0530

    add runner class for run the script

--- src/test/java/com/runner/TestRunner.java
+++ src/test/java/com/runner/TestRunner.java
@@ -1,6 +1,17 @@
 package com.runner;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features= "src//test//resources//features//CreateAccountAndSignIn.feature",
+            
+                 glue={"stepdefination","hooks"},
+                 plugin = {"pretty","html:reports/cucumber-reports.html"},
+                 dryRun=true,
+                 publish = true)
 public class TestRunner {
 
-	//ToDO : run script code write here
 }
