649a0876df83459738da117627c04d5916d49778
Archana3117 <archanamahajan1220@gmail.com>
2025-02-27 17:36:50 +0530

    add hooks class

--- src/test/java/com/hooks/Hooks.java
+++ src/test/java/com/hooks/Hooks.java
@@ -1,6 +1,55 @@
 package com.hooks;
 
-public class Hooks {
+import java.util.Properties;
 
-	//ToDO: write pre and post annotaions ,order in this class
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import com.browserfactory.DriverFactory;
+import com.util.ConfigReader;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+public class Hooks {
+	private DriverFactory driverFactory;
+	private ConfigReader configReader;
+	        Properties prop;
+
+	   
+	        
+	@Before(order = 1) 
+	public void getproperty() {
+		configReader = new ConfigReader();
+		prop = configReader.init_prop();
+	}
+
+	@Before(order = 2)
+	public void launchBrowser() {
+		String browserName = prop.getProperty("browser");
+		driverFactory = new DriverFactory();
+
+		driverFactory.init_driver(browserName);
+
+	}
+
+	@After(order = 0)
+	public void quitBrowser() {
+		//DriverFactory.getDriver().quit();
+	}
+
+	@After(order = 1)
+	public void tearDown(Scenario scenario) {
+
+		String screenshotName = null;
+
+		if (scenario.isFailed())
+
+			screenshotName = scenario.getName().replace(" ", ",");
+
+		byte[] sourcePath = ((TakesScreenshot) DriverFactory.getDriver()).getScreenshotAs(OutputType.BYTES);
+		scenario.attach(sourcePath, "image/png", screenshotName);
+	}
+
 }
