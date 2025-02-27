64801f50829bd936abd9eae46f64451e274629d8
Archana3117 <archanamahajan1220@gmail.com>
2025-02-27 17:20:17 +0530

    add ConfigReader file

--- src/main/java/com/util/ConfigReader.java
+++ src/main/java/com/util/ConfigReader.java
@@ -1,7 +1,27 @@
 package com.util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigReader {
+	private Properties prop;
 
+	public Properties init_prop() {
 
-	//ToDo: Read data from properties file here
+		prop = new Properties();
+
+		try {
+
+			FileInputStream fis = new FileInputStream(".//src//test//resources//Config//ConfigurationFile.properties");
+			prop.load(fis);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return prop;
+	}
+
 }
